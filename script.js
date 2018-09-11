02bc7db267de26a40ad9dd41db69cded127e9aab
khushboo94sudera <khushboosudera94@gmail.com>
2018-09-11 12:21:27 +0530

    changed

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 var express = require('express');
 var mysql = require('mysql');
+
+//new code
+var bodyParser = require('body-parser');
+var sessions = require('express-session');
+var session;
+
+//new code ends
+
 var app = express();
+
+//new code
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(sessions({
+    secret: '&^#*@WGKJR^#$WHGQ@Q#^^5556',
+    resave: false,
+    saveUninitialized: true
+}))
+
+
+app.get('/login', function (req, resp) {
+    session = req.session;
+    if (session.uniqueID) {
+        resp.redirect('/redirects');
+    }
+    resp.sendFile('/login/login.html', { root: __dirname });
+});
+
+app.post('/login', function (req, resp) {
+    //resp.end(JSON.stringyfy(req.body));
+    session = req.session;
+    if (session.uniqueID) {
+        resp.redirect('/redirects');
+    }
+    if (req.body.username == 'admin' && req.body.password == 'admin') {
+        session.uniqueID = req.body.username;
+    }
+    resp.redirect('/redirects');
+});
+
+app.get('/logout', function (req, resp) {
+    req.session.destroy();
+});
+
+app.get('/redirects', function (req, resp) {
+    session = req.session;
+    if (session.uniqueID) {
+        console.log(session.uniqueID);
+        resp.redirect('/admin');
+    } else {
+        resp.end('Who are you?? <a href="/logout">KILL SESSION</a>');
+    }
+});
+
+//new code ends
 
 
 var connection = mysql.createConnection({
@@ -40,7 +95,7 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
+//app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.post('/newUser', function (req, resp) {
     connection.query("INSERT INTO usertable (ID,name,email,u_name,password) values (NULL,'" + req.body.name + "','" + req.body.email + "','" + req.body.u_name + "','" + req.body.password + "')", function (error, rows, fields) {
